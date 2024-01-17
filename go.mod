21c6e6a40e9639de79bf4819f48fe09867f5bfaa
Amrit Singh <amrit0403@gmail.com>
2024-01-17 20:18:37 +0530

    Added code

---
+++ go.mod
@@ -0,0 +1,9 @@
+module blogs_graphql
+
+go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/graphql-go/graphql v0.8.1 // indirect
+	github.com/graphql-go/handler v0.2.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
+)
+
+type Blog struct {
+	ID      int64  `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+var db *sql.DB
+
+// establish a connection to the database
+func initDB() {
+	var err error
+	db, err = sql.Open("mysql", "gin_blogs:tmp_pwd@tcp(127.0.0.1:3306)/gin_blogs?charset=utf8")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Create a GraphQL object
+func createBlogType() *graphql.Object {
+	return graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "Blog",
+			Fields: graphql.Fields{
+				"id": &graphql.Field{
+					Type: graphql.Int,
+				},
+				"title": &graphql.Field{
+					Type: graphql.String,
+				},
+				"content": &graphql.Field{
+					Type: graphql.String,
+				},
+			},
+		},
+	)
+}
+
+// Define the query type for the GraphQL server
+func queryType(blogType *graphql.Object) *graphql.Object {
+	return graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "Query",
+			Fields: graphql.Fields{
+				"blogs": &graphql.Field{
+					Type: graphql.NewList(blogType),
+					Args: graphql.FieldConfigArgument{
+						"limit": &graphql.ArgumentConfig{
+							Type: graphql.Int,
+						},
+						"offset": &graphql.ArgumentConfig{
+							Type: graphql.Int,
+						},
+					},
+					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						// Read limit
+						limit, _ := p.Args["limit"].(int)
+						if limit <= 0 || limit > 20 {
+							limit = 10
+						}
+						// Read offset
+						offset, _ := p.Args["offset"].(int)
+						if offset < 0 {
+							offset = 0
+						}
+
+						return getBlogs(limit, offset)
+					},
+				},
+			},
+		},
+	)
+}
+
+func getBlogs(limit int, offset int) ([]Blog, error) {
+	var blogs []Blog
+	rows, err := db.Query("SELECT id, title, content FROM blogs limit " + strconv.Itoa(limit) + " offset " + strconv.Itoa(offset))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var b Blog
+		if err := rows.Scan(&b.ID, &b.Title, &b.Content); err != nil {
+			return nil, err
+		}
+		blogs = append(blogs, b)
+	}
+	return blogs, nil
+}
+
+func main() {
+	initDB()
+
+	blogType := createBlogType()
+
+	schema, err := graphql.NewSchema(
+		graphql.SchemaConfig{
+			Query: queryType(blogType),
+		},
+	)
+
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+
+	handler := handler.New(&handler.Config{
+		Schema: &schema,
+		Pretty: true,
+	})
+
+	http.Handle("/graphql", handler)
+	http.ListenAndServe(":8080", nil)
+}
